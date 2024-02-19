1d9bbe2829b4635aad13e084f51ef131e9ec1b8c
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-02-19 06:55:01 -0800

    .

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('my first app'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Center(
-          child: Text('HELLO NINJAS',
-              style: TextStyle(
-                fontSize: 20.0,
-                //fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                color: Colors.grey[600],
-                fontFamily: 'Anta',
-              )),
-        ),
-        backgroundColor: const Color.fromARGB(255, 56, 47, 27),
-        // floatingActionButton: const FloatingActionButton(
-        //   onPressed: () {},
-        //   child: Text('click'),
-        // ),
-      ),
+      home: Home(),
     ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text('HELLO NINJAS',
+            style: TextStyle(
+              fontSize: 20.0,
+              //fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'Anta',
+            )),
+      ),
+      backgroundColor: const Color.fromARGB(255, 56, 47, 27),
+      // floatingActionButton: const FloatingActionButton(
+      //   onPressed: () {},
+      //   child: Text('click'),
+      // ),
+    );
+  }
+}
