f6065b48ea414ca6291a9d4981aeff4f390a064e
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-02-23 09:47:40 -0800

    created quote class

---
+++ quotes/lib/quote.dart
@@ -0,0 +1,15 @@
+class Quote {
+  String text;
+  String author;
+
+  Quote({required this.text, required this.author});
+
+  // Quote(String text, String author) {
+  //   this.text = text;
+  //   this.author = author;
+  // }
+}
+
+// Quote myQuote = Quote(text:'This is a wonderful text !', author:'Author Name');
+// Quote myQuote = Quote(author:'Author Name',text:'This is a wonderful text !');
+
